5f7db720da5fa34a11caef70a2d42516eece4df0
eduardosm123 <magalhaeseduardo91@gmail.com>
2025-01-27 17:43:29 -0300

    final

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { emailSlice } from "./reducer/EmailSlice.ts";
+ 
 import { themeSlice } from "./reducer/themeSlice.ts";
 const store = configureStore({
     reducer: {
-        email: emailSlice.reducer,
+  
         theme: themeSlice.reducer
     }
 })
