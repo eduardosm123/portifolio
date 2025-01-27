32f8f7c341937128adcfcf216c445ddc7fd89f66
eduardosm123 <magalhaeseduardo91@gmail.com>
2025-01-27 17:35:07 -0300

    sem respaço restante

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -15,8 +15,8 @@ export default function Contact() {
     return (
         <>
             <NavBar>
-                <div data-testid="contact">
-                    <Box className='flex flex-col pb-1'> <Presentation text="Veja mais sobre minha Jornada como Desenvolvedor Front-end" /> </Box>
+                <div data-testid="contact" className="min-h-screen flex flex-col">
+                    <Box className='flex flex-col pb-1'> <Presentation text="Formas de entrar em contato: " /> </Box>
                     <Box className={darkMode ? "bg-[#f8fafc] hover:bg-[#f1f5f9] m-4 px-4 py-3 rounded-lg" : "bg-[#d1d5db] hover:bg-[#e5e7eb] m-4 px-4 py-3 rounded-lg"} >
                         <article>
                             <p className='font_size_contact'>
@@ -24,18 +24,13 @@ export default function Contact() {
                             </p>
                         </article>
                     </Box>
-
                     <Box className={darkMode ? "bg-[#f8fafc] hover:bg-[#f1f5f9] m-4 px-4 py-3 rounded-lg" : "bg-[#d1d5db] hover:bg-[#e5e7eb] m-4 px-4 py-3 rounded-lg"} >
                         <article>
-
                             <p className='font_size_contact'>
                                 <span className='font-bold'>Telefone / Whatsapp: </span>   <a className='text-blue-600 hover:text-red-600' href="https://wa.me/5545998381516" target="_blank" rel="noopener noreferrer">
                                     (45) 99838-1516
                                 </a>
                             </p>
-
-
-
                         </article>
                     </Box>
                     <Box className={darkMode ? "bg-[#f8fafc] hover:bg-[#f1f5f9] m-4 px-4 py-3 rounded-lg" : "bg-[#d1d5db] hover:bg-[#e5e7eb] m-4 px-4 py-3 rounded-lg"}>
@@ -44,9 +39,8 @@ export default function Contact() {
                                 <span className='font-bold'  >Linkedin:</span>  <a target='_blank' className='text-blue-600 hover:text-red-600' rel="noopener noreferrer" href='https://www.linkedin.com/in/eduardo-magalh%C3%A3es-39161a20a/'> Eduardo magalhães </a>
                             </p>
                         </article>
-                    </Box>
-                   
-                    
+                    </Box> 
+                    <div className="flex-grow"></div>
                 </div>
             </NavBar>
         </>
